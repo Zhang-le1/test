6f244edfcae5bd66355d76ec624f445da64eb535
zhangle <1010938381@qq.com>
2021-11-21 23:55:57 +0800

    逻辑运算符and、or、bool

---
+++ test_02_or.py
@@ -0,0 +1,10 @@
+"""
+练习二：定义两个整数变量 python_score、c_score，编写代码判断成绩，
+       要求只要有一门成绩 >60 就算及格
+"""
+python_score = 60
+c_score = 60
+if python_score > 60 or c_score > 60:
+    print("考试通过")
+else:
+    print("考试失败，继续努力")
