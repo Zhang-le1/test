6edd2259c534acbf59921c925883d7999ce00ec9
zhangle <1010938381@qq.com>
2021-11-26 00:39:37 +0800

    函数练习

---
+++ 03_函数/test_08_打印分割线.py
@@ -0,0 +1,23 @@
+def print_line():
+    """需求1：定义一个 print_line 函数能够打印*组成的一条分割线"""
+
+    print("*" * 50)
+
+
+print_line()
+
+
+def print_line2(char):
+    """需求2：定义一个函数能打印任意字符组成的分割线"""
+    print(char * 50)
+
+
+print_line2("-")
+
+
+def print_line3(char, times):
+    """需求4：定义一个函数能打印任意重复次数的分割线"""
+    print(char * times)
+
+
+print_line3("+", 20)
