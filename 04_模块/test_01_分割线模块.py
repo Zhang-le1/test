df93d50a8a429b6d66f6c28eb66a45c82362df1f
zhangle <1010938381@qq.com>
2021-11-26 22:51:00 +0800

    模块调用初识

---
+++ 04_模块/test_01_分割线模块.py
@@ -0,0 +1,22 @@
+def print_line3(char, times):
+    """打印单行分隔符
+
+    :param char: 分割字符
+    :param times: 字符重复次数
+    """
+    print(char * times)
+
+
+def print_lines(char, times):
+    """打印多行分割线
+
+    :param char: 分割线使用的分割字符
+    :param times: 分割符重复的次数
+    """
+    row = 0
+    while row < 5:
+        print_line3(char, times)
+        row += 1
+
+        
+name = "张乐"
---
+++ 04_模块/test_02_模块调用.py
@@ -0,0 +1,4 @@
+import test_01_分割线模块
+
+test_01_分割线模块.print_line3("-", 50)
+print(test_01_分割线模块.name)
