732ad3f4792af9389d68965051ad07b028f32e49
zhangle <1010938381@qq.com>
2021-11-28 01:13:29 +0800

    拆分和连接

---
+++ t08_字符串/test_07_拆分和连接.py
@@ -0,0 +1,19 @@
+"""
+    \r 回车
+    \t 制表符
+    \n 换行
+"""
+
+# 1、将字符串中的空白字符全部去掉
+# 2、再使用 " " 作为分隔符，拼接成一个整齐的字符串
+poem_str = "登鹳雀楼\t 王之涣 \t 白日依山尽 \t \n 黄河入海流 \t\t 欲穷千里目\n更上一层楼"
+
+print(poem_str)
+
+# 1、拆分字符串
+poem_list = poem_str.split()
+print(poem_list)
+
+# 2、合并字符串
+result = " ".join(poem_list)
+print(result)
