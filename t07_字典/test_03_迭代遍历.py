cb0b258d62ef2e7517d357cfa674b82753fc8306
zhangle <1010938381@qq.com>
2021-11-27 20:49:39 +0800

    迭代遍历

---
+++ t07_字典/test_03_迭代遍历.py
@@ -0,0 +1,9 @@
+XiaoMing_dict = {"name": "小明",
+                 "gender": True,
+                 "height": 1.75,
+                 "weight": 75.5}
+
+# 迭代遍历字典
+# 变量 k 是每一次循环中，获取到的键值对的key
+for k in XiaoMing_dict:
+    print("%s - %s" % (k, XiaoMing_dict[k]))
