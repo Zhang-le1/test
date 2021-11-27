4c8b085aa6b50eef06a534edb27782df90a088e4
zhangle <1010938381@qq.com>
2021-11-27 12:46:21 +0800

    del 列表数据排序

---
+++ t05_列表/test_04_列表数据排序.py
@@ -0,0 +1,17 @@
+name_list = ["ZhangSan", "LiSi", "WanWu", "WanXiaoEr"]
+num_list = [6, 3, 5, 8, 1]
+
+# 升序
+# name_list.sort()
+# num_list.sort()
+
+# 降序
+# name_list.sort(reverse=True)
+# num_list.sort(reverse=True)
+
+# 逆序（反转）
+name_list.reverse()
+num_list.reverse()
+
+print(name_list)
+print(num_list)
