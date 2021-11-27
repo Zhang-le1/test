50a7b914007f50a66a8c3e1f791f0a22d99bedce
zhangle <1010938381@qq.com>
2021-11-27 15:34:40 +0800

    元组和列表的转换

---
+++ t06_元组/test_04_元组和列表的转换.py
@@ -0,0 +1,10 @@
+num_list = [1, 2, 3, 4]
+print(type(num_list))
+
+# 使用 tuple（）将列表转换成元组
+num_tuple = tuple(num_list)
+print(type(num_tuple))
+
+# 使用 list（）将元组转换成列表
+num_list2 = list(num_tuple)
+print(type(num_list2))
